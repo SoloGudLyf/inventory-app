92a209025fc364a04b48d03f94214f969ebaf393
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-26 22:00:08 +0000

    feat: Prepare right sql statements
    To display books in their categories

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
-import { getAllBooks } from "../db/query.js";
+import { getBooksByCategory } from "../db/query.js";
 
 const homeController = async (req, res) => {
-  const books = await getAllBooks();
+  const books = await getBooksByCategory("Self help");
   res.send(books);
 };
 
--- db/query.js
+++ db/query.js
@@ -1,10 +1,17 @@
 import { pool } from "./pool.js";
 
-async function getAllBooks() {
-  const { rows } = await pool.query("SELECT * FROM books_info");
+async function getBooksByCategory(category) {
+  const { rows } = await pool.query(
+    "SELECT * FROM books_info JOIN book_categories ON books_info.category=book_categories.id WHERE book_categories.category=$1",
+    [category]
+  );
   console.log(rows);
 
   return rows;
 }
 
-export { getAllBooks };
+async function getAllCategories() {
+  const { rows } = await pool.query("SELECT * FROM book_categories");
+  return rows;
+}
+export { getBooksByCategory, getAllCategories };

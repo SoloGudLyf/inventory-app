7130f9df1926957f8b35d6ad7f8ab4a999bd6fa6
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-26 20:59:48 +0000

    feat: Setup views folder for frontend

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { userRouter } from "./routes/userRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+app.set("view engine", "ejs");
+prompt.set("view", path.join(__dirname, "views"));
 app.get("/", userRouter);
 
 app.listen(3000, () => {

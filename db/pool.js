04fb54b79420491cd779cb75fbcca61abd34437f
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-28 18:46:05 +0000

    feat: configure app to be work on railway

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_PUBLIC_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL,
 });
 
 export { pool };
